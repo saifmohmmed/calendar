790fdaa369c8e120a9ff6f65d10877d0ac1917a8
saifmohmmed <saifmohmmed80@gmail.com>
2020-07-14 22:52:25 -0400

    Add files via upload

---
+++ js/Calendar.js
@@ -0,0 +1,130 @@
+/*
+Assignment 2: Calendar (part 2) Calendar class JS
+ AUTHOR: Saif Badr
+         07/07/2020 
+*/
+
+class Calendar
+{
+    constructor(year =null, month=null, date = null)
+    {
+        this.time = new Date(Date.now());
+        if(!(year == undefined || year == null || month == undefined || month == null || date == undefined || date == null))
+        {
+            this.year = year;
+            this.month = month;
+            this.date = date;
+        }
+        else
+        {
+            this.time = new Date(Date.now());
+            this.year =  year == null ? this.time.getFullYear() : year;
+            this.month = month == null ? this.time.getMonth() + 1: month;
+            this.day = this.time.getDay() + 1;
+            this.date = date == null ? this.time.getDate() : date;
+        }
+        this.currentMonth = this.time.getMonth() + 1;
+        this.currentYear = this.time.getFullYear();
+    }
+
+    setDate(day)
+    {
+        this.date = day;
+    }
+
+    set(year, month, day)
+    {
+        this.year = year;
+        this.month = month;
+        this.date = day;
+    }
+
+    setMonth(month)
+    {
+        this.month = month;
+    }
+
+    setYear(year)
+    {
+        this.year = year;
+    }
+
+    getDaysOfMonth()
+    {
+        return new Date(this.year, this.month, 0).getDate();
+    }
+
+    getFirstDayOfMonth()
+    {
+        return new Date(this.year, this.month-1, 1).getDay() + 1;
+    }
+
+    nextMonth() 
+    {
+        if(this.month +1 >12) 
+        {
+            this.month = 1;
+            this.year = this.year + 1;
+        }
+        else
+        {
+            this.month = this.month+1;
+        }
+    }
+
+    previousMonth() 
+    {
+        if(this.month - 1 < 1) 
+        {
+            this.month = 12;
+            this.year = this.year - 1;
+        }
+        else
+        {
+            this.month = this.month - 1;
+        }
+    }
+
+    getLastDayOfMonth()
+    {
+        return new Date(this.year, this.month, 0).getDay() + 1;
+    }
+
+    getLastDateOfPreviousMonth()
+    {
+        return new Date(this.year, this.month-1, 0).getDate();
+    }
+
+    getMonthString()
+    {
+        switch(this.month)
+        {
+            case 1:  return "January";
+            case 2:  return "February";
+            case 3:  return "March";
+            case 4:  return "April";
+            case 5:  return "May";
+            case 6:  return "June";
+            case 7:  return "July";
+            case 8:  return "August";
+            case 9:  return "September";
+            case 10: return "October";
+            case 11: return "November";
+            case 12: return "December";
+        }
+    }
+
+    getDayString()
+    {
+        switch(this.day)
+        {
+            case 1:  return "Sunday";
+            case 2:  return "Monday";
+            case 3:  return "Tuesday";
+            case 4:  return "Wednesday";
+            case 5:  return "Thursday";
+            case 6:  return "Friday";
+            case 7:  return "Saturday";
+        }
+    }
+}
---
+++ js/main.js
@@ -0,0 +1,88 @@
+/*
+Assignment 2: Calendar (part 2)
+ AUTHOR: Saif Badr
+         07/07/2020 
+*/
+
+var calendar = {}; //global object
+
+//Load JSON file
+$.ajax({
+    url : 'holydays.json',
+    type : 'get',
+    async : false
+}).then(data =>{
+    calendar.holidays = data;
+}).catch(data=>{
+    console.error(data);
+}).always(()=>{initiate()});
+
+// registering event handlers
+document.addEventListener("DOMContentLoaded", main, false);
+
+// entry point
+function main()
+{
+    calendar.calDate = document.getElementById("date");
+    calendar.calMonth = document.getElementById("month_name");
+    calendar.cal = new Calendar();
+    initiate();
+}
+
+function initiate()
+{
+    calendar.calMonth.innerHTML = calendar.cal.getMonthString() + " " + calendar.cal.year;
+    let dateCount = calendar.cal.getDaysOfMonth();
+    let firstDay = calendar.cal.getFirstDayOfMonth();
+    let lastDay = calendar.cal.getLastDayOfMonth();
+    let prevLastDate = calendar.cal.getLastDateOfPreviousMonth();
+    let html = "";
+
+    for(let i = (firstDay - 1); i >= 1; --i)
+    {
+        let date = prevLastDate - i + 1;
+        html += "<div id='otherMonthDate" + date + "' class='otherMonthDate'>" + date + "</div>";
+    }
+
+    for(let i = 1; i <= dateCount; ++i)
+    {
+        var holyday = getHoliday(calendar.cal.year, calendar.cal.month, i);
+        html += "<div id='currentMonthDate" + i + "' class='currentMonthDate";
+        if(i == calendar.cal.date && calendar.cal.currentMonth == calendar.cal.month && calendar.cal.currentYear == calendar.cal.year){
+            html += " today";
+        }
+        if(holyday != undefined) {
+            html += " holyday";
+        }
+        html += "'>" + i ;
+        if(holyday != undefined) {
+            html+= "<p>"+holyday+"</p>";
+        }
+        html+="</div>";
+    }
+
+    for(let i = lastDay + 1, z = 1; i <= 7; ++i, ++z)
+    {
+        html += "<div id='otherMonthDate" + z + "' class='otherMonthDate'>" + z + "</div>";
+    }
+
+    calendar.calDate.innerHTML = html;
+}
+
+function getHoliday(year, month, day) {
+    var holyday;
+    try {
+        holyday = calendar.holidays == undefined ? undefined : calendar.holidays[year][month][day];
+    }catch (e){}
+    return holyday;
+}
+
+document.getElementById('previous').addEventListener('click',function(){
+    calendar.cal.previousMonth();
+    initiate();
+});
+
+document.getElementById('next').addEventListener('click', function(){
+    calendar.cal.nextMonth();
+    initiate();
+});
